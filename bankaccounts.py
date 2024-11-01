88eafaa801102fdf61c14f26b6323e4552c00760
bradyDarren <dcgbfinancial@gmail.com>
2024-11-01 06:14:22 -0500

    addition of methods within the bankaccount class

---
+++ bankaccounts.py
@@ -0,0 +1,33 @@
+
+class Account:
+    def __init__(self):
+        pass
+
+class Checkings: 
+    def __init__(self):
+        pass
+
+    def get_check_bal(self):
+        pass
+
+class Savings: 
+    def __init__(self):
+        pass
+    
+    def get_save_bal(self):
+        pass
+
+class CreditCard: 
+    def __init__(self):
+        pass
+
+    def cc_interest(self):
+        pass
+
+class AutoLoan: 
+    def __init__(self):
+        pass
+
+    def auto_interest(self):
+        pass
+
