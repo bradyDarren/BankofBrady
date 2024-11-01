2f45ce28286516e2c16f4f654152c90a795bc4cb
bradyDarren <dcgbfinancial@gmail.com>
2024-11-01 06:02:05 -0500

    test functionality of class methods

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-import customers
+from customers import Customers
+
+first_name = input("first_name: ")
+last_name = input("last_name: ")
+social = input("social: ")
+phone = input("phone: ")
+email = input("email: ")
+citizen = input("citizen: ")
+
+customer_info = Customers(f_name=first_name, l_name=last_name, 
+                          ssn=social, phone_num=phone,email=email,us_cit=citizen)
+
+print(customer_info.f_name, customer_info.l_name)
+
+customer_info.name_change(change=True, new_name="Johnson")
+
+print(customer_info.f_name,customer_info.l_name)
