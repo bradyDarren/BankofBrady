d92af1a582eb276576e73707d4fa2876feef9f45
bradyDarren <dcgbfinancial@gmail.com>
2024-11-01 05:21:43 -0500

    creation of classes

--- customers.py
+++ customers.py
@@ -1,4 +1,21 @@
 
 class Customers: 
-    def __init__(self, f_name, ):
-        pass
+    def __init__(self, f_name, l_name, ssn):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.ssn = ssn
+
+class Address: 
+    def __init__(self, street_address, city, zip):
+        self.self = self
+        self.street_address = street_address
+        self.city = city
+        self.zip = zip
+
+class DriversLicense: 
+    def __init__(self, dl_num, state, exp_date, iss_date):
+        self.dl_num = dl_num
+        self.state = state
+        self.exp_date = exp_date
+        self.iss_date = iss_date
+        
