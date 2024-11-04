3971c3a14c853a74ed222d138270d33733a2d719
bradyDarren <dcgbfinancial@gmail.com>
2024-11-04 17:33:33 -0600

    commit

---
+++ data.py
@@ -0,0 +1,10 @@
+customer_info = [
+    {"account number": {123456789 : {"first name":"carl",
+                                    "last name":"jackson",
+                                    "ssn":1234856790, 
+                                    "phone number": 1234567890,
+                                    "email": "abcd@gmail.com",
+                                    "citizen": True }}}
+]
+
+print(customer_info[0]["account number"])
