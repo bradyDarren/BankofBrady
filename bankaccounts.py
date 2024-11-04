16d715f0eb4028c533d00eadd8d85f59bf93210e
bradyDarren <dcgbfinancial@gmail.com>
2024-11-04 17:31:31 -0600

    addition of methods

--- bankaccounts.py
+++ bankaccounts.py
@@ -1,14 +1,14 @@
 
 class Account:
-    def __init__(self):
-        pass
+    def __init__(self, account_num):
+        self.account_num = account_num
 
 class Checkings: 
-    def __init__(self):
-        pass
+    def __init__(self, checking_bal):
+        self.checking_bal = checking_bal
 
-    def get_check_bal(self):
-        pass
+    def get_check_bal(self, checking_bal):
+        return f"The balacne of our Checking account is ${float(checking_bal)}."
 
 class Savings: 
     def __init__(self):
@@ -31,3 +31,6 @@ class AutoLoan:
     def auto_interest(self):
         pass
 
+class MortageLoan:
+    def __init__(self):
+        pass
